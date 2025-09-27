c9df670907a48ede29c6ad54d3ddc5f6dff74582
Julian De Maio <julidema82@gmail.com>
2025-09-27 20:28:16 -0300

    add player entity

--- src/main/kotlin/com/spprj/unq_dapps/_s2_GrupoG/model/Player.kt
+++ src/main/kotlin/com/spprj/unq_dapps/_s2_GrupoG/model/Player.kt
@@ -1,7 +1,19 @@
 package com.spprj.unq_dapps._s2_GrupoG.model
 
-data class Player(
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "players")
+open class Player(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false)
+    val teamId: String,
+
+    @Column(nullable = false)
     val name: String,
+
     val matchesPlayed: Int,
     val goals: Int,
     val assists: Int,
