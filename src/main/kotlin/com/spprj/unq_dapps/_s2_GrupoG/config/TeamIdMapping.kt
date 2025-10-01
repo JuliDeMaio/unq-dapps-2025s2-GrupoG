d94845c75997715b86f2e1f9146eb3097553cdd1
Julian De Maio <julidema82@gmail.com>
2025-10-01 17:39:22 -0300

    add id mapping from whoscore to football data

---
+++ src/main/kotlin/com/spprj/unq_dapps/_s2_GrupoG/config/TeamIdMapping.kt
@@ -0,0 +1,26 @@
+package com.spprj.unq_dapps._s2_GrupoG.config
+
+object TeamIdMapping {
+    val whoScoredToFootballData = mapOf(
+        "26" to "64",   // Liverpool
+        "167" to "65",  // Manchester City
+        "15" to "61",   // Chelsea
+        "13" to "57",   // Arsenal
+        "30" to "73",   // Tottenham
+        "32" to "66",   // Manchester United
+        "31" to "62",   // Everton
+        "23" to "67",   // Newcastle
+        "24" to "58",   // Aston Villa
+        "14" to "338",  // Leicester
+        "18" to "340",  // Southampton
+        "29" to "563",  // West Ham
+        "162" to "354", // Crystal Palace
+        "211" to "397", // Brighton
+        "170" to "63",  // Fulham
+        "189" to "402", // Brentford
+        "174" to "351", // Nottingham Forest
+        "184" to "328", // Burnley
+        "163" to "356", // Sheffield United
+        "161" to "76"   // Wolves
+    )
+}
