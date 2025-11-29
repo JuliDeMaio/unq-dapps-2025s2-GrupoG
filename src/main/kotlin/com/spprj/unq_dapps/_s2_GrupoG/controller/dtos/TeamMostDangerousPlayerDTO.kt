c9e05339f165019bbeaf2e1e38cb5c06a9c4321e
Julian De Maio <julidema82@gmail.com>
2025-11-28 22:35:47 -0300

    new dtos for dangerScore

---
+++ src/main/kotlin/com/spprj/unq_dapps/_s2_GrupoG/controller/dtos/TeamMostDangerousPlayerDTO.kt
@@ -0,0 +1,7 @@
+package com.spprj.unq_dapps._s2_GrupoG.controller.dtos
+
+data class TeamMostDangerousPlayerDTO(
+    val teamName: String,
+    val mostDangerousPlayer: String,
+    val dangerScore: Double
+)
